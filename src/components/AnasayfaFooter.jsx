8ae0cc6e2e4c751727300479e5b9eacf901df601
Elif Cetin <xxelifcetinxx@gmail.com>
2025-03-23 22:46:45 +0300

    anasayfafooter done

--- src/components/AnasayfaFooter.jsx
+++ src/components/AnasayfaFooter.jsx
@@ -8,19 +8,19 @@ export default function Footer() {
         <div className="solFooter">
           <div className="yaziFooter">Teknolojik Yemekler</div>
           <div className="solFooterYazi" style={{ width: "55%" }}>
-            <img src="https://s3-alpha-sig.figma.com/img/0908/44d1/bf8ba15bccac8d589cd96d428c644077?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=oLWTxBKpzn-tKU~-upT1ckWsl~8YJo~-I2ruMEPpjviA~IWnFHcJeJcPVX1-O5WWYY~GwNuy6tRCaJp8WOB39YkATYywR~G1fTlh52Vj1YN5wb4P4BywV7a8tFoFFmIay9tM45bASGC6OtQx4W8wELvPcc1gIhLhBEuW9Jo8OiUt~s3msOmvvg8pyDsC86-i32LrAP5gPXlIcbsh1VcDVecQ7GyiLx1N-BsKkz25rxG~0A6Ids-CjvmrfBIv1wbRerXokri8ujkEYcThXdHhEadp5wfIhKqBosPQIHbb8BjluQ8A6IdOczKErURZhA01RfiQ7OJWfNj19OzJNWpKsw__" />
+          <img alt="icon-1" src="./images/iteration-2-images/footer/icons/icon-1.png"/>
             341 Londonderry Road, Istanbul Türkiye
           </div>
           <div className="solFooterYazi">
-            <img src="https://s3-alpha-sig.figma.com/img/9fff/c412/dba32836ccb7135781a5ffed3a177067?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WUdda1G1fag1vE87nyOmd8Li4Eue4G0C-Y8lVT-CYMDa1sARLGMWo2WHIB2Juzn5XwGSsf8JHXTQjyBBsjkp49YCgO09sU2gYtqAyc91BPrIB9xAF4Q29gWubPc8NArd4Jn5adTw7yAUH7s01n7bBkRfAbLwU5A4f4xIUS1x52dZg3JrPk2jEHPXZ0K3pTq5YpmrBFfl6UUhiTS0UJM-2C1AQtbUAFOHIQZYGCNPgQFG8Edf1Vs66~ux~bOwjyjArWjBZ~GuSCZQhbvP5SqL0c-pZbFphO3Y0ykxtFiU1lkHAQ0tKxuvzJoJRFzOaGgA6oNKXXfSwHKEYaenc9dsdw__" />
+          <img alt="icon-2" src="./images/iteration-2-images/footer/icons/icon-2.png"/>
             aciktim@teknolojikyemekler.com
           </div>
           <div className="solFooterYazi">
-            <img src="https://s3-alpha-sig.figma.com/img/5bcf/42e8/cf44624c37cb7290e61fd24e95f5f71c?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BpkSW5OTIOjZWE0L~ZRL7HUxGebrhrMT3gRUXqJUTuKwos-mWk94OXsVTSoKqWq9lPyKsAgjncXdDJ7xPVEky~f~SC4px-g4k37eQWfOGzlP7E5NYFMaFsDEohR4W6YpgaWGWwrIuRKRJfatDoKyGf2L8iMm82sgh9BwMPqEJvO6dqQ3IpPXEoSEqI93L2kv7MTgfCX3Hil52wKZoJRvURkvp4oHXUdcMlOlN8e8S3uc~r8qrQPrJkex2ExJTeJTHlPE6xCjK56b-gRaa2bmConnsEytTPYsfIitYEfEXBnrQTLEhDYPf-7KXtxWvvDpPXMVH9BzanrZ68N5wiml4w__" />
+          <img alt="icon-3" src="./images/iteration-2-images/footer/icons/icon-3.png"/>
             +90 216 123 45 67
           </div>
         </div>
-        <div className="ortaFotter">
+        <div className="ortaFooter">
           <h3>Hot Menu</h3>
           <div>Terminal Pizza</div>
           <div>5 Kişilik Hackathlon Pizza</div>
@@ -29,33 +29,15 @@ export default function Footer() {
           <div>Testler Geçti Mutlu Burger</div>
           <div>Position Absolute Acı Burger</div>
         </div>
-        <div className="sagFotter">
+        <div className="sagFooter">
           <h3>Instagram</h3>
           <div>
-            <img
-              src="https://s3-alpha-sig.figma.com/img/6e4d/a4aa/684f85ad3b4d28ccf28088950926ba03?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XfUoI6FjBVOIKolOe~JuAJvY4gn~u0RBE9GZ4ZH6hYlX8YeUncGWX87OJWylfVSFhkWE1UxN2GlWJOYNbBQubium9mXTODgT7uvCr1Y6l2BdhbTzKJbARR29Z5uPkEGQWhFkpkEKsXmgziw6UJ72dqiVHElriW64ElBidu22JDeBemHB1qv7cIyPcdy84DxOIsBX21raL4c~Cp-H5QHkl2VyvzzNrBdSuN~NozaS6u9dWU-JXHVcjIv2loPLxKRqihax~~k9Iqz2Zk3GX08OdZvMhP1cTdqpUzdhht5e2jHGiQwVhP1K0ogtZ9qM5G40ZmN7awvYY9A3vJ51-XNeng__"
-              alt="pizza1"
-            />
-            <img
-              src="https://s3-alpha-sig.figma.com/img/ab8f/b0dd/d3d48d4bfbb94368f413eea6e51a731e?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EzHOT-S5jBLQIcR2HsAGINtU1DX~ojKLtMd9mbnRXH~Nh-GeTVTtVjWjlGWzJBXlBbW~4pzOUA60lb3xkqqH3DJ0dxo6yVOXQiBMGThttXsW4a22gdnWX5KvfuCW-Zt59LKfecZWPEfnjhjDfGTTLFtqFQcrd31pujKhpMLNigMSGP01UvPVf1IketQ4S4VsVWJKxsH-aDBTIj5P0C8pwhveVPAO37Qf7OkOtw5sMV4lHXGW75aam~Q~8-vg9vl5q1dkMwBjA99mYZ8or5ilUqTPfEB1lK-T~DoPtHhSzRsK3xDJSEYkjxKoH7MD796QwZgcuWotIbZsApKE2QZEmw__"
-              alt="pizza2"
-            />
-            <img
-              src="https://s3-alpha-sig.figma.com/img/758e/c71c/3340af96d90783c9a2fc83d7eba5e435?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EdTNthP8BQmTO5-sarfmjIOaPPN2HdVAu-OOv6uZt6OS0v9vv6CQjHLzVi-t2fvMkHwKdgtjGvX8KFX80qK8gXV14EnaH4SFg5GCfLbdt09CGMaSjvyl3HhsHdR4pL9qu7bcNy8i4Aep5VFrebyd3G925JolHWaf4aIH2Jf6JEnQB-G-1BowGslazSOoTMfBHouSVzPGpb980die0aoe4sv~OOYf8g~kZmWp1S6LuLrA0c7CJ2wHBc0-l2Khs4g~MXVOLFumLqjWoFPyUwtY8-154mVY-2dSLKHqPPqsjFKngNV19lD4m-bXCeMM3DRwqTNAp0rDNuKMO7jJ6BMmDQ__"
-              alt="pizza3"
-            />
-            <img
-              src="https://s3-alpha-sig.figma.com/img/cff3/c2ac/3faa7e879f7e8774c45ed7c70887e593?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SZVVIJOaO3~1t4dxGg9pUYWfbzOHkrHygbHlpB28WXJvfOkg7AGZ06~tGKocffHKmMFdPmHhorEGwzmHUGwM2mPdU94eHAaCw23tGJYfxF7fRktxvY5NJvhXiP2QTrfJOKIe95wugLCMcq43A4BoBOD8iPwB3~W2OZ20bz8CtwIcZDRSg4e~z1wnPCNeYsaTR87mrK0iyxfK1QaL85DdYfivfPeeUHqvjV44FeaZUmb7pB~ByMuvw4z3LqBAcv4Jc2j-tkKlUPOV-7xOAUa9fxFly2XS3ZQ9~mdzZMLr7xKK4J~vfSQQwRyBcwO9LXYeiODfWkqgl5ZkbRCPT6jYhQ__"
-              alt="pizza4"
-            />
-            <img
-              src="https://s3-alpha-sig.figma.com/img/fac5/a071/ac57e061246eb6dc85915d553dd69139?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QtCMqfUu1baAhpuwVAdJwvnAEzg4kcAoSidWe4fg~8eNkJ0jncQed6XI7sm0U-O7MflAPnp~g-KRASRWEDwuOj3mJ2BV54meyHSdrSBYudm8LbLgdythoYanaoIqL78ZnUeb1CrLWjvE5dMOh6xvFT7GYTFzDW2pBsd~x9YO0BtM-BGKLzCtf7QlrRWbzQZ0dXZAtjnc2Ext3S3pNFHLfSmmWVeh5uBtqXk6Y~h-bSiJkdafZMb1AVUfS~-lfepisk1BkdfsKH8djDZe8WG4I0z65NkZkMbgaRn3M8rrA~sV-rnvuDlPlYGcDz6sVIZJALM~1OCgUx9aomG9AB20Ww__"
-              alt="pizza5"
-            />
-            <img
-              src="https://s3-alpha-sig.figma.com/img/6604/67f8/a72f04200cdc712df4da9dbe5d2cc740?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=O6eTsc4~59CCl3Jq4aAA6HnJp6jUQr504oCHyWFjdGjnR~TLG7NHooG51v0LUCXZNOc~Wf21cRpR6w5sMVrmLrbeRnawSfy~UmZD4liJd61Mlk2uscRGwVBwA7jWmtGCmtyA4THP5JoqgFHbM3qF0VowSdJsSigZe-OjzGqYZ1Q~X2ExMwEKcPBUlE8weAnQ3cnVlzhbgYXHtsVWa1P92ceuPI8-YKxFLpR-JHtAP-UV1ygKTtj6k8wZm4jHknqGogs73DatTRivgiFj8iQ7~14OcZCXH2iKSkFcUNUlhKJCcZYgoUcTQculkZOw613y0dqSqN7KGoCLYbWO~y5Qdw__"
-              alt="pizza6"
-            />
+            <img alt="li-0" src="./images/iteration-2-images/footer/insta/li-0.png"/>
+            <img alt="li-1" src="./images/iteration-2-images/footer/insta/li-1.png"/>
+            <img alt="li-2" src="./images/iteration-2-images/footer/insta/li-2.png"/>
+            <img alt="li-3" src="./images/iteration-2-images/footer/insta/li-3.png"/>
+            <img alt="li-4" src="./images/iteration-2-images/footer/insta/li-4.png"/>
+            <img alt="li-5" src="./images/iteration-2-images/footer/insta/li-5.png"/>
           </div>
         </div>
       </div>
@@ -63,7 +45,7 @@ export default function Footer() {
         <div className="info">
           <div>© 2025 Teknolojik Yemekler - @elifcetin </div>
           <div>
-            <TwitterLogo size={32} weight="fill" />
+            <TwitterLogo size={25} weight="fill" />
           </div>
         </div>
       </div>
